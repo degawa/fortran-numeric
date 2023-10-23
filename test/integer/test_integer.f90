24d588e8dfa23b548271c85a7ab2500ec9dfd685
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-10-23 10:39:01 +0900

    refactored unit tests for is_positive/negative

--- test/integer/test_integer.f90
+++ test/integer/test_integer.f90
@@ -63,106 +63,142 @@ contains
         type(error_type), allocatable, intent(out) :: error
             !! error handler
 
+        integer(int8), allocatable :: input_int8(:)
+        integer(int16), allocatable :: input_int16(:)
+        integer(int32), allocatable :: input_int32(:)
+        integer(int64), allocatable :: input_int64(:)
+        logical, allocatable :: expected(:)
+        integer(int32) :: test_num
+        character(256) :: buffer
+
         ! int8
-        call check(error, is_positive(1_int8), "expected 1_int8 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(Int8_Max), "expected 127_int8 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(0_int8), "expected 0_int8 is positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(-1_int8), "expected -1_int8 is not positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(Int8_Min), "expected -128_int8 is not positive but not")
-        if (allocated(error)) return
+        input_int8 = [integer(int8) :: 1, Int8_Max, 0, -1, Int8_Min]
+        expected = [.true., .true., .true., .false., .false.]
+        do test_num = 1, size(input_int8)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int8(test_num), &
+                "_int8 is ", logical_to_msg(is_positive(input_int8(test_num))), &
+                " but not"
+            call check(error, is_positive(input_int8(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
 
         ! int16
-        call check(error, is_positive(1_int16), "expected 1_int16 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(Int16_Max), "expected 32767_int16 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(0_int16), "expected 0_int16 is positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(-1_int16), "expected -1_int16 is not positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(Int16_Min), "expected -32768_int16 is not positive but not")
-        if (allocated(error)) return
+        input_int16 = [integer(int16) :: -1, 1, Int16_Max, 0, Int16_Min]
+        expected = [.false., .true., .true., .true., .false.]
+        do test_num = 1, size(input_int16)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int16(test_num), &
+                "_int16 is ", logical_to_msg(is_positive(input_int16(test_num))), &
+                " but not"
+            call check(error, is_positive(input_int16(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
 
         ! int32
-        call check(error, is_positive(1_int32), "expected 1_int32 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(Int32_Max), "expected 2147483647_int32 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(0_int32), "expected 0_int32 is positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(-1_int32), "expected -1_int32 is not positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(Int32_Min), "expected -2147483648_int32 is not positive but not")
-        if (allocated(error)) return
+        input_int32 = [integer(int32) :: 0, -1, 1, Int32_Max, Int32_Min]
+        expected = [.true., .false., .true., .true., .false.]
+        do test_num = 1, size(input_int32)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int32(test_num), &
+                "_int32 is ", logical_to_msg(is_positive(input_int32(test_num))), &
+                " but not"
+            call check(error, is_positive(input_int32(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
 
         ! int64
-        call check(error, is_positive(1_int64), "expected 1_int64 is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(Int64_Max), "expected 9223372036854775807_int64  is positive but not")
-        if (allocated(error)) return
-        call check(error, is_positive(0_int64), " expected 0_int64 is positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(-1_int64), "expected -1_int64 is not positive but not")
-        if (allocated(error)) return
-        call check(error,.not. is_positive(Int64_Min), "expected -9223372036854775808_int64 is not positive but not")
-        if (allocated(error)) return
+        input_int64 = [integer(int64) :: Int64_Min, -1, 0, 1, Int64_Max]
+        expected = [.false., .false., .true., .true., .true.]
+        do test_num = 1, size(input_int64)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int64(test_num), &
+                "_int64 is ", logical_to_msg(is_positive(input_int64(test_num))), &
+                " but not"
+            call check(error, is_positive(input_int64(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
+    contains
+        pure function logical_to_msg(l) result(msg)
+            logical, intent(in) :: l
+            character(:), allocatable :: msg
+            if (l) then
+                msg = "positive"
+            else
+                msg = "not positive"
+            end if
+        end function logical_to_msg
     end subroutine test_is_positive
 
     subroutine test_is_negative(error)
         type(error_type), allocatable, intent(out) :: error
             !! error handler
 
+        integer(int8), allocatable :: input_int8(:)
+        integer(int16), allocatable :: input_int16(:)
+        integer(int32), allocatable :: input_int32(:)
+        integer(int64), allocatable :: input_int64(:)
+        logical, allocatable :: expected(:)
+        integer(int32) :: test_num
+        character(256) :: buffer
+
         ! int8
-        call check(error,.not. is_negative(1_int8), "expected 1_int8 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(Int8_Max), "expected 127_int8 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(0_int8), "expected 0_int8 is negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(-1_int8), "expected -1_int8 is not negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(Int8_Min), "expected -128_int8 is not negative but not")
-        if (allocated(error)) return
+        input_int8 = [integer(int8) :: 1, Int8_Max, 0, -1, Int8_Min]
+        expected = [.false., .false., .false., .true., .true.]
+        do test_num = 1, size(input_int8)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int8(test_num), &
+                "_int8 is ", logical_to_msg(is_negative(input_int8(test_num))), &
+                " but not"
+            call check(error, is_negative(input_int8(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
 
         ! int16
-        call check(error,.not. is_negative(1_int16), "expected 1_int16 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(Int16_Max), "expected 32767_int16 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(0_int16), "expected 0_int16 is negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(-1_int16), "expected -1_int16 is not negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(Int16_Min), "expected -32768_int16 is not negative but not")
-        if (allocated(error)) return
+        input_int16 = [integer(int16) :: -1, 1, Int16_Max, 0, Int16_Min]
+        expected = [.true., .false., .false., .false., .true.]
+        do test_num = 1, size(input_int16)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int16(test_num), &
+                "_int16 is ", logical_to_msg(is_negative(input_int16(test_num))), &
+                " but not"
+            call check(error, is_negative(input_int16(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
 
         ! int32
-        call check(error,.not. is_negative(1_int32), "expected 1_int32 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(Int32_Max), "expected 2147483647_int32 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(0_int32), "expected 0_int32 is negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(-1_int32), "expected -1_int32 is not negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(Int32_Min), "expected -2147483648_int32 is not negative but not")
-        if (allocated(error)) return
+        input_int32 = [integer(int32) :: 0, -1, 1, Int32_Max, Int32_Min]
+        expected = [.false., .true., .false., .false., .true.]
+        do test_num = 1, size(input_int32)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int32(test_num), &
+                "_int32 is ", logical_to_msg(is_negative(input_int32(test_num))), &
+                " but not"
+            call check(error, is_negative(input_int32(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
 
         ! int64
-        call check(error,.not. is_negative(1_int64), "expected 1_int64 is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(Int64_Max), "expected 9223372036854775807_int64  is negative but not")
-        if (allocated(error)) return
-        call check(error,.not. is_negative(0_int64), " expected 0_int64 is negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(-1_int64), "expected -1_int64 is not negative but not")
-        if (allocated(error)) return
-        call check(error, is_negative(Int64_Min), "expected -9223372036854775808_int64 is not negative but not")
-        if (allocated(error)) return
+        input_int64 = [integer(int64) :: Int64_Min, -1, 0, 1, Int64_Max]
+        expected = [.true., .true., .false., .false., .false.]
+        do test_num = 1, size(input_int64)
+            write (buffer, '(A,I0,A,A,A)') &
+                "expected ", input_int64(test_num), &
+                "_int64 is ", logical_to_msg(is_negative(input_int64(test_num))), &
+                " but not"
+            call check(error, is_negative(input_int64(test_num)), expected(test_num), trim(buffer))
+            if (allocated(error)) return
+        end do
+    contains
+        pure function logical_to_msg(l) result(msg)
+            logical, intent(in) :: l
+            character(:), allocatable :: msg
+            if (l) then
+                msg = "negative"
+            else
+                msg = "not negative"
+            end if
+        end function logical_to_msg
     end subroutine test_is_negative
 
     subroutine test_to_string(error)
